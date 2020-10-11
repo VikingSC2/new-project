2dc88f8c4a3c8b3654e348752df9236673c3c5bf
VikingSC2 <61349976+VikingSC2@users.noreply.github.com>
2020-10-11 12:54:03 +0300

    uplouding file
    1234

---
+++ Viking-git/APP.js
@@ -0,0 +1,19 @@
+// let dict ={
+// Ярик: "16 лет", 
+// Егор: "13 лет",
+// Андрей: "54 года",
+// 123: 311,
+// 911: "police"
+// }
+// console.log(dict[123],[911]);       
+let numArr = [10,9,1,2,99,101,66,33,28,54];
+
+numArr.sort(function(a,b){ 
+    return a - b     
+    
+})
+// for (let index = 0; index < numArr.length; index++) {
+    
+//     console.log(numArr[0]);
+// }
+console.log(numArr)    
